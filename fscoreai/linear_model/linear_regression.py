bf0e01df94650aee789b20f257e6b9cbd6f9842b
Arvinder Singh <sardarchitect@gmail.com>
2023-02-20 22:00:57 -0500

    Update linear regression model to include gradient descent and multiple linear regression

--- fscoreai/linear_model/linear_regression.py
+++ fscoreai/linear_model/linear_regression.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-class CustomLinearRegression():
+class SimpleLinearRegression():
     """Linear Regression through Ordinary Least Squares (OLS).
     
-    Linear Regression fits a linear hyperplane with coefficients 
-    coef_ = (coef_1,...,coef_d). It minimizes the cost by minimizing the 
-    sum of the residuals between observed inputs and the predicted outputs.
+    Linear Regression fits a linear hyperplane with a single coefficient
+    (coef_) and an intercept (intercept_). It minimizes the cost by minimizing
+    the sum of the residuals between observed inputs and the predicted outputs.
 
     Parameters
     ----------
@@ -22,7 +22,7 @@ class CustomLinearRegression():
         self.coef_ = 0
         self.intercept_ = 0
 
-    def fit_stats(self, X, y):
+    def fit(self, X, y, fit_type = 'stat'):
         """"
         Parameters
         ----------
@@ -35,12 +35,28 @@ class CustomLinearRegression():
         ------
         null
         """
-        X_mean = np.mean(X, axis=0)
-        y_mean = np.mean(y)
+        if (fit_type == 'stat'):
+            X_mean = np.mean(X, axis=0)
+            y_mean = np.mean(y)
+            self.coef_ = np.sum((X - X_mean).T*(y-y_mean), axis=1)/(np.sum((X - X_mean)**2, axis=0)) 
+            self.intercept_ = y_mean - (self.coef_ * X_mean)
+            return self
+        
+        if (fit_type == 'grad'):
+            self.alpha = 1e-11  #   Learning rate
+            self.epochs = 50
+            self.n, self.d = X.shape
 
-        self.coef_ = np.sum((X - X_mean).T*(y-y_mean), axis=1)/(np.sum((X - X_mean)**2, axis=0)) 
-        # self.intercept_ = y_mean - (self.coef_ * X_mean)
+            for epoch in range(self.epochs):
+                print("Epoch:", epoch, "Coef:", self.coef_, "Intercept:", self.intercept_)
+                y_pred = self.predict(X)
+                d_coef = - 2 / self.n * np.sum(np.dot((y - y_pred),X))        #    Derivative w.r.t. self.coef_
+                d_intercept = - 2 / self.n * np.sum(y - y_pred)         #    Derivative w.r.t. self.intercept_
+                self.coef_ -=  self.alpha * d_coef          #    Update self.coef_ 
+                self.intercept_ -=  self.alpha * d_intercept  #    Update self.intercept_
 
+                print("\n d_coef", d_coef, "d_intercept", d_intercept)
+            return self
 
     def predict(self, X):
         """"
@@ -55,3 +71,60 @@ class CustomLinearRegression():
             N dimensional list of model predictions
         """
         return np.dot(X, self.coef_) + self.intercept_
+
+class MultipleLinearRegression():
+    # Implementation reference: https://towardsdatascience.com/linear-regression-using-python-b136c91bf0a2
+    """Linear Regression Using Gradient Descent.
+    Parameters
+    ----------
+    eta : float
+        Learning rate
+    n_iterations : int
+        No of passes over the training set
+    Attributes
+    ----------
+    w_ : weights/ after fitting the model
+    cost_ : total error of the model after each iteration
+    """
+
+    def __init__(self, eta=0.05, n_iterations=1000):
+        self.eta = eta
+        self.n_iterations = n_iterations
+
+    def fit(self, x, y):
+        """Fit the training data
+        Parameters
+        ----------
+        x : array-like, shape = [n_samples, n_features]
+            Training samples
+        y : array-like, shape = [n_samples, n_target_values]
+            Target values
+        Returns
+        -------
+        self : object
+        """
+
+        self.cost_ = []
+        self.w_ = np.zeros((x.shape[1], 1))
+        m = x.shape[0]
+
+        for _ in range(self.n_iterations):
+            y_pred = np.dot(x, self.w_)
+            residuals = y_pred - y
+            gradient_vector = np.dot(x.T, residuals)
+            self.w_ -= (self.eta / m) * gradient_vector
+            cost = np.sum((residuals ** 2)) / (2 * m)
+            self.cost_.append(cost)
+        return self
+
+    def predict(self, x):
+        """ Predicts the value after the model has been trained.
+        Parameters
+        ----------
+        x : array-like, shape = [n_samples, n_features]
+            Test samples
+        Returns
+        -------
+        Predicted value
+        """
+        return np.dot(x, self.w_)
