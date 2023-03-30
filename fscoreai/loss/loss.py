72b41be8385819db4dd189b3cc4d2fe32b33c00c
Arvinder Singh <sardarchitect@gmail.com>
2023-03-29 21:23:13 -0400

    Added regression evaluation metrics

--- fscoreai/loss/loss.py
+++ fscoreai/loss/loss.py
@@ -1,4 +1,25 @@
 import numpy as np
 
+def squared_error(a, b):
+    return np.sum(np.square(a - b))
+
 def mean_squared_error(a, b):
     return np.sum(np.square(a - b))/a.shape[0]
+
+def mean_absolute_error(a, b):
+    return np.sum(np.absolute(a - b))/a.shape[0]    
+
+def root_mean_squared_error(a, b):
+    return np.sqrt(mean_squared_error(a, b))
+
+def root_mean_squared_log_error(a, b):
+    return np.log(np.sqrt(mean_squared_error(a, b)))
+    
+def r_squared(y, y_pred):
+    y_mean = y.copy()
+    for i in range(y.shape[0]):
+        y_mean[i] = np.mean(y)
+
+    SSM = squared_error(y, y_mean)
+    SSR = squared_error(y, y_pred)
+    return 1 - (SSR / SSM)
